e6f6c95611d50d39e3bba57b16b44e4248261f25
PrabhupadaPradhan <72605687+PrabhupadaPradhan@users.noreply.github.com>
2022-03-14 17:57:53 +0530

    Update numerology_4.py

--- numerology_4.py
+++ numerology_4.py
@@ -20,6 +20,10 @@ pdf = FPDF()
 df = pd.DataFrame()
 df["Name"] = pd.Series([name_org])
 df["Date"] = pd.Series([date_org])
+compat_dict = {"A" : {1 : [4, 5, 7], 2 : [4, 6, 22], 3 : [3, 6, 8, 9], 4 : [1, 2, 7, 8], 5 : [1], 6 : [2, 3, 6, 9], 7 : [1, 4, 22], 8 : [4, 22], 9 : [3, 6, 9], 11 : [22], 22 : [2, 7, 8, 11]},
+              "B" : {1 : [1, 8], 2 : [2, 3, 7, 9, 11], 3 : [2, 22], 4 : [4, 6, 22], 5 : [5, 9, 11], 6 : [4, 11], 7 : [2, 7, 11], 8 : [1, 11], 9 : [2, 5, 7, 11, 22], 11 : [2, 5, 6, 7, 8, 9, 11], 22 : [3, 4, 9, 22]},
+              "C" : {1 : [2, 6, 22], 2 : [1, 5], 3 : [4, 5, 7, 11], 4 : [3, 11], 5 : [2, 3, 4, 6, 7, 22], 6 : [1, 5, 7, 8], 7 : [3, 5, 6, 9], 8 : [3, 6, 8, 9], 9 : [4, 8], 11 : [3, 4], 22 : [1, 5]},
+              "D" : {1 : [3, 9, 11], 2 : [8], 3 : [1], 4 : [5, 9], 5 : [8], 6 : [22], 7 : [8], 8 : [2, 5, 7], 9 : [1], 11 : [1], 22 : [6]}}
 life_dict = {1 : "Independence",
              2 : "Cooperation",
              3 : "Self-expression",
@@ -163,7 +167,6 @@ if export_as_pdf:
                 date = sum([int(i) for i in str(date)])
                 if date in [2, 4, 11, 22]:
                     a_life_pn = date
-            st.title(str(a_life_pn))
             life_pn = sum([int(j) for j in (str(df['Date'][i]).split(' '))[0].split('-')])
             while life_pn >= 10:
                 life_pn = sum([int(k) for k in str(life_pn)])
@@ -173,6 +176,15 @@ if export_as_pdf:
                 date = sum([int(i) for i in str(date)])
                 if date in [2, 4, 11, 22]:
                     life_pn = date
+            if a_life_pn in compat_dict["A"][life_pn]:
+                compat = "A"
+            elif a_life_pn in compat_dict["B"][life_pn]:
+                compat = "B"
+            elif a_life_pn in compat_dict["C"][life_pn]:
+                compat = "C"
+            elif a_life_pn in compat_dict["D"][life_pn]:
+                compat = "D"
+            st.title(f'compatability : {compat}')
             if name in item[1]:
                 date = str(df['Date'][i]).split(' ')[0].split('-')
                 day = int(date[2])
